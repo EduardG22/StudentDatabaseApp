cbc6b5527ad46143650116e4fe89c67288f22a24
EduardG22 <popaeduard22@gmail.com>
2023-11-01 22:22:37 +0100

    Adding the Enroll in courses method

--- src/studentdatabaseapp/Student.java
+++ src/studentdatabaseapp/Student.java
@@ -6,11 +6,14 @@ public class Student {
     private String firstName;
     private String lastName;
     private int gradeYear;
-    private int studentID;
-    private String courses;
-    private String tuitionBalance;
-    private int costOfCourse = 600;
+    private String studentID;
+    private String courses = null;
+    private int tuitionBalance = 0;
+    private static int costOfCourse = 600;
+    private static int id = 1000;
 
+
+    // Constructor
     public Student() {
         Scanner in = new Scanner(System.in);
         System.out.println("Enter student first name: ");
@@ -19,17 +22,41 @@ public class Student {
         System.out.println("Enter student last name: ");
         this.lastName = in.nextLine();
 
-        System.out.println("1 - Freshmen\n2 - for Sophmore\n3 - Junior\n4 - Senior\nEnter student grade level: ");
+        System.out.println("1 - Freshmen\n2 - Sophmore\n3 - Junior\n4 - Senior\nEnter student grade level: ");
         this.gradeYear = in.nextInt();
-        System.out.println(firstName + " " + lastName + " " + gradeYear);
+        setStudentID();
+        System.out.println(firstName + " " + lastName + " " + gradeYear + " " + studentID);
+
+
     }
 
 
-    // Constructor
+
 
     // Generate ID
+    private void setStudentID(){
+        // Grade Level + ID
+        id++;
+        this.studentID =  gradeYear + "" + id;
+    }
 
     // Enroll in courses
+    public void enroll(){
+        // Get inside a loop, user hits 0
+        do {
+            System.out.print("Enter course to enroll (Q to quit): ");
+            Scanner in = new Scanner(System.in);
+            String course = in.nextLine();
+            if (!course.equals("Q")) {
+                courses = courses + "\n" + course;
+                tuitionBalance = tuitionBalance + costOfCourse;
+            } else {break;}
+        } while ( 1 != 0);
+
+        System.out.println("ENROLLED IN: " + courses);
+        System.out.println("TUITION BALANCE: " + tuitionBalance);
+
+    }
 
     // View balance
 
